8a225afd75502648cdd21f7b89a2d060c78110b2
Oliver Jeppesen <oliverjeppesen123@icloud.com>
2023-09-27 13:41:44 +0200

    opdelt MainContent til AddPip

--- src/components/AddPip.jsx
+++ src/components/AddPip.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AddButton from './addButton';
+import { Pips } from '../entities/pips';
 
-export default function AddPip() {
+export default function AddPip({pips, setPips}) {
+  const [username, setUsername] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  
+  
+  const handleAddPip = () => {
+    const newPip = new Pips(username, content, new Date());
+    setPips([...pips, newPip]);
+
+    // Tømmer input felter
+    setUsername("");
+    setContent("");
+  };
+
+
   return (
-    <div>AddPip</div>
+    <div>
+      <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <input
+            type="text"
+            placeholder="Content"
+            value={content}
+            onChange={handleContentChange}
+          />
+          <AddButton buttonText={"Add Pip"} onClick={handleAddPip}></AddButton>
+    </div>
   )
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,46 +2,17 @@ import { useState } from "react";
 import PipBox from "./PipBox";
 import { Pips } from "../entities/pips";
 import AddButton from "./addButton";
+import AddPip from "./AddPip";
 
 export default function MainContent() {
-  const [username, setUsername] = useState("");
-  const [content, setContent] = useState("");
   const [pips, setPips] = useState([]);
 
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-  };
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handleAddPip = () => {
-    const newPip = new Pips(username, content, new Date());
-    setPips([...pips, newPip]);
-
-    // Tømmer input felter
-    setUsername("");
-    setContent("");
-  };
 
   return (
     <main>
       <div className="list">
         <div className="add-container">
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <input
-            type="text"
-            placeholder="Content"
-            value={content}
-            onChange={handleContentChange}
-          />
-          <AddButton buttonText={"Add Pip"} onClick={handleAddPip}></AddButton>
+          <AddPip pips={pips} setPips={setPips}/>
         </div>
         {pips.map((pip, index) => {
             return <PipBox key={index} pip={pip} />;
