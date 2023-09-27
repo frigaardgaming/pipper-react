c9810770e07c88767d6fe575952fbdf0b6b0d35d
Oliver Jeppesen <oliverjeppesen123@icloud.com>
2023-09-27 15:38:30 +0200

    PipBox changed from label to list

--- src/components/PipBox.jsx
+++ src/components/PipBox.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function PipBox({ pip }) {
   return (
-    <article className="pips">
-      <label htmlFor={pip.content}>
-        <strong>{pip.username}</strong>: {pip.content}
-      </label>
-    </article>
+    <div className="pips">
+      <ul>
+        <li>
+          <strong>{pip.username}</strong>: {pip.content}
+        </li>
+      </ul>
+    </div>
   );
 }
