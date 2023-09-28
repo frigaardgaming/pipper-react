7198b702a34e00611d6cfd44830f7bd2c0df4c2d
Oliver Jeppesen <oliverjeppesen123@icloud.com>
2023-09-28 12:35:10 +0200

    added some useful styling

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,18 +7,17 @@ import AddPip from "./AddPip";
 export default function MainContent() {
   const [pips, setPips] = useState([]);
 
-
   return (
     <main>
       <div className="list">
         <div className="add-container">
-          <AddPip pips={pips} setPips={setPips}/>
+          <AddPip pips={pips} setPips={setPips} />
         </div>
-        <>
-        {pips.map((pip, index) => {
+        <div className="pip-container">
+          {pips.map((pip, index) => {
             return <PipBox key={index} pip={pip} />;
-        })}
-        </>
+          })}
+        </div>
       </div>
     </main>
   );
