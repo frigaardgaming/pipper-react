55c2a685dcfa1b05235a58b5c26a7914abfc91ab
FrederiKoza <115544821+FrederiKoza@users.noreply.github.com>
2023-09-28 11:23:36 +0200

    pipbox

--- src/components/PipBox.jsx
+++ src/components/PipBox.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "../styles/PipBox.css";
 
 export default function PipBox({ pip }) {
   return (
     <div className="pips">
       <ul>
         <li>
-          <strong>{pip.username}</strong>: {pip.content}
+          <strong>{pip.username}</strong>
+          <p>{pip.content}</p>
         </li>
       </ul>
     </div>
